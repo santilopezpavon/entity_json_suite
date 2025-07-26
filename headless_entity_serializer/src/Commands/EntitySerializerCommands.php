e666f94be995f78e0bc3dccfb55350bfadfe0d23
Santi López <s.lopez_@outlook.com>
2025-07-26 14:04:47 +0200

    add comments

--- headless_entity_serializer/src/Commands/EntitySerializerCommands.php
+++ headless_entity_serializer/src/Commands/EntitySerializerCommands.php
@@ -2,27 +2,32 @@
 
 namespace Drupal\headless_entity_serializer\Commands;
 
+use Drupal\headless_entity_serializer\Service\Generator\GeneratorService;
 use Drush\Commands\DrushCommands;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Drupal\headless_entity_serializer\Service\EntitySerializerService;
 
 /**
  * Drush commands for serializing Drupal entities to JSON files.
  */
 class EntitySerializerCommands extends DrushCommands {
 
-  protected $generator;
   /**
-   * The entity serializer service.
+   * The generator service for serializing entities.
    *
-   * @var \Drupal\headless_entity_serializer\Service\EntitySerializerService
+   * @var \Drupal\headless_entity_serializer\Service\Generator\GeneratorService
    */
-  protected $entitySerializerService;
+  protected $generator;
 
-  public function __construct($generator) {
+  /**
+   * Constructs a new EntitySerializerCommands object.
+   *
+   * This is the recommended way to inject services into Drush commands.
+   *
+   * @param \Drupal\headless_entity_serializer\Service\Generator\GeneratorService $generator
+   *   The generator service.
+   */
+  public function __construct(GeneratorService $generator) {
     $this->generator = $generator;
   }
-
 
   /**
    * Fully regenerates JSON files for selected entity types.
@@ -36,16 +41,8 @@ class EntitySerializerCommands extends DrushCommands {
    * Fully regenerates JSON files for all configured entity types.
    */
   public function fullRegenerate() {
-    $this->io()->section('Ejecutando regeneración completa');
+    $this->io()->section('Executing full regeneration of serialized entities and aliases');
     $this->generator->fullGenerate();
-
-    /*$result = $this->entitySerializerService->fullRegenerate();
-
-    if ($result['status']) {
-      $this->io()->success($result['message']);
-    } else {
-      $this->io()->error($result['message']);
-    }*/
   }
 
   /**
@@ -60,15 +57,8 @@ class EntitySerializerCommands extends DrushCommands {
    * Performs an incremental update for configured entity types.
    */
   public function incrementalUpdate() {
-    $this->io()->section('Ejecutando actualización incremental');
+    $this->io()->section('Executing incremental update of serialized entities and aliases');
     $this->generator->incrementalGenerate();
-
-/*
-    if ($result['status']) {
-      $this->io()->success($result['message']);
-    } else {
-      $this->io()->error($result['message']);
-    } */
   }
 
 }
